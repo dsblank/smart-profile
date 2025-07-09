7bbf5e4ea6bb317afc2dbfad597181b722723ba8
Douglas Blank <doug.blank@gmail.com>
2025-07-09 05:44:33 -0400

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,11 @@
+def banner():
+    print("Picture")
+
+def activities():
+    print("Activities")
+
+def opik_summary():
+    print("Opik Summary")
+
+def em_summary():
+    print("Experiment Management")
