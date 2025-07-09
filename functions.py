fdaf3553c7484d8c892bacc1318b7005b8e936ec
Douglas Blank <doug.blank@gmail.com>
2025-07-09 11:11:25 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -182,40 +182,41 @@ def opik_summary():
         st.markdown('</div>', unsafe_allow_html=True)
 
 def em_summary():
-    st.html("<b>Experiment Management<b>")
-    if comet_ml_api_key:
-        #workspaces = api_ml.get_workspaces()
-        experiments = api_ml.get_panel_experiments()
-        st.html(f"* {len(experiments)} Experiments")
-    else:
-        st.html("* Unknown experiments (set Comet ML API key)")
-    projects = api_ml.get(workspace=github_name)[:3]
-    if (projects):
-        project_links = [f'https://www.comet.com/{github_name}/{projects[0]}', 
-                         f'https://www.comet.com/{github_name}/{projects[1]}', 
-                         f'https://www.comet.com/{github_name}/{projects[2]}']
-        st.markdown(f"• **New experiments in EM projects:** [{projects[0]}]({project_links[0]}), [{projects[1]}]({project_links[1]}), [{projects[2]}]({project_links[2]})")
-    else:
-        st.markdown(f"• **New experiments in EM projects:** nothing new")
-        
-
-    models = api_ml.get_registry_model_names(workspace=github_name)[:3]
-    if (models):
-        model_links = [f'https://www.comet.com/{github_name}/model-registry/{models[0]}',
-                      f'https://www.comet.com/{github_name}/model-registry/{models[1]}',
-                      f'https://www.comet.com/{github_name}/model-registry/{models[2]}']
-        st.markdown(f"• **Changes in EM models:** [{models[0]}]({model_links[0]}), [{models[1]}]({model_links[1]}), [{models[2]}]({model_links[2]})")
-    else:
-        st.markdown(f"• **Changes in EM models:** nothing new")
-
-    artifacts = api_ml.get_artifact_list(workspace=github_name)['artifacts'][:3]
-    if (artifacts):
-        artifact_names = []
-        for artifact in artifacts:
-            artifact_names.append(artifact["name"])
-        artifact_links = [f'https://www.comet.com/{github_name}/artifacts/{artifact_names[0]}',
-                         f'https://www.comet.com/{github_name}/artifacts/{artifact_names[1]}',
-                         f'https://www.comet.com/{github_name}/artifacts/{artifact_names[2]}']
-        st.markdown(f"• **Updates in EM artifacts:** [{artifact_names[0]}]({artifact_links[0]}), [{artifact_names[1]}]({artifact_links[1]}), [{artifact_names[2]}]({artifact_links[2]})")
-    else:
-        st.markdown(f"• **Updates in EM artifacts:** nothing new")
+    st.markdown("### 🔬 Experiment Management")
+    with st.container(border=True):
+        if comet_ml_api_key:
+            #workspaces = api_ml.get_workspaces()
+            experiments = api_ml.get_panel_experiments()
+            st.html(f"* {len(experiments)} Experiments")
+        else:
+            st.html("* Unknown experiments (set Comet ML API key)")
+        projects = api_ml.get(workspace=github_name)[:3]
+        if (projects):
+            project_links = [f'https://www.comet.com/{github_name}/{projects[0]}', 
+                             f'https://www.comet.com/{github_name}/{projects[1]}', 
+                             f'https://www.comet.com/{github_name}/{projects[2]}']
+            st.markdown(f"• **New experiments in EM projects:** [{projects[0]}]({project_links[0]}), [{projects[1]}]({project_links[1]}), [{projects[2]}]({project_links[2]})")
+        else:
+            st.markdown(f"• **New experiments in EM projects:** nothing new")
+            
+    
+        models = api_ml.get_registry_model_names(workspace=github_name)[:3]
+        if (models):
+            model_links = [f'https://www.comet.com/{github_name}/model-registry/{models[0]}',
+                          f'https://www.comet.com/{github_name}/model-registry/{models[1]}',
+                          f'https://www.comet.com/{github_name}/model-registry/{models[2]}']
+            st.markdown(f"• **Changes in EM models:** [{models[0]}]({model_links[0]}), [{models[1]}]({model_links[1]}), [{models[2]}]({model_links[2]})")
+        else:
+            st.markdown(f"• **Changes in EM models:** nothing new")
+    
+        artifacts = api_ml.get_artifact_list(workspace=github_name)['artifacts'][:3]
+        if (artifacts):
+            artifact_names = []
+            for artifact in artifacts:
+                artifact_names.append(artifact["name"])
+            artifact_links = [f'https://www.comet.com/{github_name}/artifacts/{artifact_names[0]}',
+                             f'https://www.comet.com/{github_name}/artifacts/{artifact_names[1]}',
+                             f'https://www.comet.com/{github_name}/artifacts/{artifact_names[2]}']
+            st.markdown(f"• **Updates in EM artifacts:** [{artifact_names[0]}]({artifact_links[0]}), [{artifact_names[1]}]({artifact_links[1]}), [{artifact_names[2]}]({artifact_links[2]})")
+        else:
+            st.markdown(f"• **Updates in EM artifacts:** nothing new")
